caf9d191d9411b319f6ad6f84ad250d41270318f
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-07-21 09:38:53 +0530

    Time: 136 ms (18.02%), Space: 175.8 MB (11.71%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        vector<pair<char, int>> str;
+        s = '1' + s + '1';
+        int len = 1;
+        for(int i=1;i<s.size();i++) {
+            if(s[i] != s[i-1]) {
+                str.push_back({s[i-1], len});
+                len = 1;
+            }
+            else {
+                len++;
+            }
+        }
+        char ch = s.back();
+        str.push_back({ch, len});
+        int ans = 0;
+        int n = str.size();
+        for(int i=2;i<=n-2;i++) {
+            int leftGain, rightGain;
+            if(str[i].first == '1' && str[i-1].first == str[i+1].first) {
+                leftGain = str[i-1].second;
+                rightGain = str[i+1].second;
+            }
+            int sum = leftGain + rightGain;
+            ans = max(ans, sum);
+        }
+        int session = 0;
+        for(int i=0;i<n;i++) {
+            if(str[i].first == '1') session += str[i].second;
+        }
+        return ans + session - 2;
+    }
+};
