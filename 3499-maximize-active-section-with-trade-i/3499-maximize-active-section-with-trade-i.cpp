51d58f8ac277010843ada7bbab22d131de850340
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-07-22 22:45:15 +0530

    Time: 144 ms (15.18%), Space: 175.9 MB (8.48%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -5,20 +5,20 @@ public:
         s = '1' + s + '1';
         int len = 1;
         for(int i=1;i<s.size();i++) {
-            if(s[i] != s[i-1]) {
-                str.push_back({s[i-1], len});
-                len = 1;
+            if(s[i] == s[i-1]) {
+                len++;
             }
             else {
-                len++;
+                str.push_back({s[i-1], len});
+                len = 1;
             }
         }
         char ch = s.back();
         str.push_back({ch, len});
         int ans = 0;
         int n = str.size();
-        for(int i=2;i<=n-2;i++) {
-            int leftGain, rightGain;
+        int leftGain, rightGain;
+        for(int i=2;i<=n-2;i+=2) {
             if(str[i].first == '1' && str[i-1].first == str[i+1].first) {
                 leftGain = str[i-1].second;
                 rightGain = str[i+1].second;
