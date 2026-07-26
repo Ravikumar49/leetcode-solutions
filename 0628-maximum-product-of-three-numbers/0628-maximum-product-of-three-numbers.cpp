be3e40a2b4b354ac1cd91a2bc09f5387ca397513
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-07-26 12:11:16 +0530

    Time: 0 ms (100%), Space: 31.5 MB (46.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int prod1 = nums[n-1] * nums[n-2] * nums[n-3];
-        int prod2 = nums[0] * nums[1] * nums[n-1];
+        if(n == 3) return nums[0] * nums[1] * nums[2];
+        int largest = INT_MIN, secondLargest = INT_MIN + 1, thirdLargest = INT_MIN + 2, smallest = INT_MAX, secondSmallest = INT_MAX - 1;
+        for(int i=0;i<n;i++) {
+            if(nums[i] >= largest) {
+                thirdLargest = secondLargest;
+                secondLargest = largest;
+                largest = nums[i];
+            }
+            else if(nums[i] >= secondLargest && nums[i] < largest) {
+                thirdLargest = secondLargest;
+                secondLargest = nums[i];
+            }
+            else if(nums[i] >= thirdLargest && nums[i] < secondLargest) {
+                thirdLargest = nums[i];
+            }
+            if(nums[i] <= smallest) {
+                secondSmallest = smallest;
+                smallest = nums[i];
+            }
+            else if(nums[i] <= secondSmallest && nums[i] > smallest) {
+                secondSmallest = nums[i];
+            }
+        }
+        int prod1 = largest * secondLargest * thirdLargest;
+        int prod2 = smallest * secondSmallest * largest;
         return max(prod1, prod2);
     }
 };
