ca0cbc2eed8c35e0e808523986896cdcd841d589
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-07-31 18:34:25 +0530

    Time: 0 ms (100%), Space: 11.3 MB (18.97%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -2,26 +2,12 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        vector<int> copy = nums;
-        sort(copy.begin(), copy.end());
-        vector<int> indexes;
+        int drop = 0;
         for(int i=0;i<n;i++) {
-            if(copy[0] == nums[i]) {
-                indexes.push_back(i);
+            if(nums[i] > nums[(i+1) % n]) {
+                drop++;
             }
         }
-        bool flag = true;
-        for(int i=0;i<indexes.size();i++) {
-            int index = indexes[i];
-            flag = true;
-            for(int j=0;j<n;j++) {
-                if(copy[j] != nums[(index + j) % n]) {
-                    flag = false;
-                }
-                if(!flag) break;
-            }
-            if(flag) return flag;
-        }
-        return false;
+        return drop <= 1;
     }
 };
