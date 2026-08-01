f7644a1a71f76b99fe0b01b1e4214a58653b44be
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-01 11:31:27 +0530

    Time: 82 ms (25.25%), Space: 9.6 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    int solve(int left, int right, vector<int>& nums) {
-        if(left == right) return nums[left];
-        int takeLeft = nums[left] - solve(left+1, right, nums);
-        int takeRight = nums[right] - solve(left, right-1, nums);
-        return max(takeLeft, takeRight);
+    int maxScore(int i, int j, int turn, vector<int>& nums) {
+        if(i == j) return nums[i];
+        if(turn == 0) {
+            int s1 = nums[i] + maxScore(i+1, j, 1, nums);
+            int s2 = nums[j] + maxScore(i, j-1, 1, nums);
+            return max(s1, s2);
+        }
+        else {
+            int s1 = maxScore(i+1, j, 0, nums);
+            int s2 = maxScore(i, j-1, 0, nums);
+            return min(s1, s2);
+        }
     }
     bool predictTheWinner(vector<int>& nums) {
-        return solve(0, nums.size()-1, nums) >= 0;
+        int sum = 0;
+        for(int x:nums) {
+            sum += x;
+        }
+        int p1_score = maxScore(0, nums.size()-1, 0, nums);
+        int p2_score = sum - p1_score;
+        return p1_score>=p2_score;
     }
 };
