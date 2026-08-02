481f5c4ef92fe9c21b4a10a27ec54aaf47553095
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-02 18:26:58 +0530

    Time: 1 ms (32.83%), Space: 11.3 MB (12.85%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    long long rec(long long n) {
+    long long rec(long long n, unordered_map<long long, int>& dp) {
         if(n == 1) return 0;
-        if(n%2 == 0) return 1 + rec(n/2);
-        return 1 + min(rec(n-1), rec(n+1));
+        if(dp.find(n) != dp.end()) return dp[n];
+        int steps;
+        if(n%2 == 0) steps = 1 + rec(n/2, dp);
+        else {
+            steps = 1 + min(rec(n-1, dp), rec(n+1, dp));
+        }
+        return dp[n] = steps;
     }
     int integerReplacement(int n) {
-        return rec(n);
+        unordered_map<long long, int> dp;
+        return rec(n, dp);
     }
 };
