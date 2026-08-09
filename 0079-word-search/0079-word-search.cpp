0c2f41397e49e6827ae90a754b406dce02db6c0f
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-09 16:12:20 +0530

    Time: 1834 ms (5.01%), Space: 381.7 MB (5.03%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& grid, string word, int r, int c, vector<vector<bool>>& visited, int j) {
+        if(j == word.size()) return true;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        visited[r][c] = true;
+        for(auto [dr, dc] : dir) {
+                int nr = r + dr;
+                int nc = c + dc;
+                if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && grid[nr][nc] == word[j]) {
+                    visited[nr][nc] = true;
+                    if(dfs(grid, word, nr, nc, visited, j+1)) {
+                        return true;
+                    }
+                    else {
+                        visited[nr][nc] = false;
+                    }
+                }
+        }
+        visited[r][c] = false;
+        return false;
+    }
+    bool exist(vector<vector<char>>& grid, string word) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == word[0]) {
+                    bool isPossible = dfs(grid, word, i, j, visited, 1);
+                    if(isPossible) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
