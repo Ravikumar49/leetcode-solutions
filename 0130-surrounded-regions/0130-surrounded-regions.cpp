e75473771a8bed285b26e4cc0b7e7ce622019056
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-09 18:39:11 +0530

    Time: 12 ms (6.09%), Space: 18 MB (6.12%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-    vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    void dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, int r, int c) {
-        int m = board.size();
-        int n = board[0].size();
-        for(auto [dr, dc] : dir) {
-            int nr = r + dr;
-            int nc = c + dc;
-            if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && board[nr][nc] == 'O') {
-                visited[nr][nc] = true;
-                dfs(board, visited, nr, nc);
-            }
-        }
-    }
     void solve(vector<vector<char>>& board) {
         int m = board.size();
         int n = board[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        for(int j=0;j<n;j++) {
-            if(board[0][j] == 'O') {
-                visited[0][j] = true;
-                dfs(board, visited, 0, j);
-            }
-            if(board[m-1][j] == 'O') {
-                visited[m-1][j] = true;
-                dfs(board, visited, m-1, j);
+        queue<pair<int, int>> q;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(board[0][j] == 'O') {
+                    q.push({0, j});
+                }
+                if(board[m-1][j] == 'O') {
+                    q.push({m-1, j});
+                }
+                if(board[i][0] == 'O') {
+                    q.push({i, 0});
+                }
+                if(board[i][n-1] == 'O') {
+                    q.push({i, n-1});
+                }
             }
         }
-        for(int i=0;i<m;i++) {
-            if(board[i][0] == 'O') {
-                visited[i][0] = true;
-                dfs(board, visited, i, 0);
-            }
-            if(board[i][n-1] == 'O') {
-                visited[i][n-1] = true;
-                dfs(board, visited, i, n-1);
+        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        while(!q.empty()) {
+            int size = q.size();
+            for(int i=0;i<size;i++) {
+                auto[r, c] = q.front();
+                visited[r][c] = true;
+                q.pop();
+                for(auto [dr, dc] : dir) {
+                    int nr = r + dr;
+                    int nc = c + dc;
+                    if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && board[nr][nc] == 'O') {
+                        visited[nr][nc] = true;
+                        q.push({nr, nc});
+                    }
+                }
             }
         }
         for(int i=1;i<m-1;i++) {
