ad3724125d6d46fb6589bfe13143fda831250ed0
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-10 22:05:42 +0530

    Time: 35 ms (14.14%), Space: 18.8 MB (36.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,25 +3,28 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<int, int>> q;
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        int count = 0;
+        queue<pair<int, int>> q;
         vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int count = 0;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
                 if(grid[i][j] == '1' && !visited[i][j]) {
-                    q.push({i, j});
                     count++;
+                    q.push({i, j});
                     while(!q.empty()) {
-                        auto [r, c] = q.front();
-                        visited[r][c] = true;
-                        q.pop();
-                        for(auto [dr, dc] : dir) {
-                            int nr = r + dr;
-                            int nc = c + dc;
-                            if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && grid[nr][nc] == '1') {
-                                visited[nr][nc] = true;
-                                q.push({nr, nc});
+                        int size = q.size();
+                        for(int i=0;i<size;i++) {
+                            auto [r, c] = q.front();
+                            q.pop();
+                            visited[r][c] = true;
+                            for(auto [dr, dc] : dir) {
+                                int nr = r + dr;
+                                int nc = c + dc;
+                                if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && grid[nr][nc] == '1') {
+                                    visited[nr][nc] = true;
+                                    q.push({nr, nc});
+                                }
                             }
                         }
                     }
