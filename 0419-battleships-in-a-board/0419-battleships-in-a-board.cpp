ce60a71ffde64ff37c89de600ed6b079247495ab
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-11 10:11:20 +0530

    Time: 0 ms (100%), Space: 13.7 MB (10.81%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<pair<int, int>> q;
+        vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int count = 0;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(board[i][j] == 'X' && !visited[i][j]) {
+                    count++;
+                    q.push({i, j});
+                    while(!q.empty()) {
+                        auto[r, c] = q.front();
+                        q.pop();
+                        visited[r][c] = true;
+                        for(auto [dr, dc] : dir) {
+                            int nr = r + dr;
+                            int nc = c + dc;
+                            if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && board[nr][nc] == 'X') {
+                                q.push({nr, nc});
+                                visited[nr][nc] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
