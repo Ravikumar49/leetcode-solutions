9e2ac086947c609e1234e2abf392e29a24f5ad2c
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-12 10:01:23 +0530

    Time: 219 ms (62.97%), Space: 11.9 MB (12.95%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    bool dfs(vector<vector<char>>& grid, string word, int r, int c, vector<vector<bool>>& visited, int j) {
+    vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    bool dfs(vector<vector<char>>& board, string word, vector<vector<bool>>& visited, int r, int c, int j) {
         if(j == -1) return true;
-        int m = grid.size();
-        int n = grid[0].size();
+        int m = board.size();
+        int n = board[0].size();
         visited[r][c] = true;
         for(auto [dr, dc] : dir) {
-                int nr = r + dr;
-                int nc = c + dc;
-                if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && grid[nr][nc] == word[j]) {
-                    visited[nr][nc] = true;
-                    if(dfs(grid, word, nr, nc, visited, j-1)) {
-                        return true;
-                    }
-                    else {
-                        visited[nr][nc] = false;
-                    }
+            int nr = r + dr;
+            int nc = c + dc;
+            if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && board[nr][nc] == word[j]) {
+                visited[nr][nc] = true;
+                if(dfs(board, word, visited, nr, nc, j-1)) {
+                    return true;
                 }
+                else {
+                    visited[nr][nc] = false;
+                }
+            }
         }
         visited[r][c] = false;
         return false;
     }
-    bool exist(vector<vector<char>>& grid, string word) {
-        int m = grid.size();
-        int n = grid[0].size();
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         for(int i=m-1;i>=0;i--) {
             for(int j=n-1;j>=0;j--) {
-                if(grid[i][j] == word.back()) {
-                    bool isPossible = dfs(grid, word, i, j, visited, word.size() - 2);
+                if(board[i][j] == word.back()) {
+                    bool isPossible = dfs(board, word, visited, i, j, word.size() - 2);
                     if(isPossible) return true;
                 }
             }
