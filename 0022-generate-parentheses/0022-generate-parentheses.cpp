6be543ac033651317e7c4988e2566b5a122723f5
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-14 21:31:47 +0530

    Time: 3 ms (68.94%), Space: 16.3 MB (18.16%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void backtrack(int n, int open, int close, string curr, vector<string>& res) {
+        if(open == n && close == n) {
+            res.push_back(curr);
+            return;
+        }
+        if(open < n) {
+            curr.push_back('(');
+            backtrack(n, open+1, close, curr, res);
+            curr.pop_back();
+        }
+        if(close < open) {
+            curr.push_back(')');
+            backtrack(n, open, close+1, curr, res);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        string curr;
+        backtrack(n, 0, 0, curr, res);
+        return res;
+    }
+};
