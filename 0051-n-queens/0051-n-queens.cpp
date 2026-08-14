9614179ba9814142c52811ea48a6980d9241c093
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-14 14:14:29 +0530

    Time: 3 ms (61.49%), Space: 10.2 MB (59.66%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isSafe(vector<string>& board, int row, int col) {
+        for(int r=0;r<row;r++) {
+            for(int c=0;c<board.size();c++) {
+                if(board[r][c] == 'Q') {
+                    int rowDiff = abs(row - r);
+                    int colDiff = abs(col - c);
+                    if(rowDiff == colDiff) return false;
+                }
+            }
+        }
+        return true;
+    }
+    void backtrack(int row, int n, vector<bool>& visited, vector<string>& board, vector<vector<string>>& res) {
+        if(row == n) {
+            res.push_back(board);
+            return;
+        }
+        for(int col=0;col<n;col++) {
+            if(!visited[col] && isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                visited[col] = true;
+                backtrack(row+1, n, visited, board, res);
+                visited[col] = false;
+                board[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> board(n, string(n, '.'));
+        vector<bool> visited(n, false);
+        backtrack(0, n, visited, board, res);
+        return res;
+    }
+};
