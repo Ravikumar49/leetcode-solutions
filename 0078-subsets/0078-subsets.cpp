20b48ce0d10b3493d8bb95cced3de3f556fdc823
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-14 09:49:12 +0530

    Time: 0 ms (100%), Space: 9.8 MB (96.57%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    void createSubset(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& res) {
-        if(index == nums.size()) {
-            res.push_back(current);
+    void backtrack(vector<int>& nums, int i, vector<int>& curr, vector<vector<int>>& res) {
+        if(i == nums.size()) {
+            res.push_back(curr);
             return;
         }
-        current.push_back(nums[index]);
-        createSubset(nums, index+1, current, res);
-        current.pop_back();
-        createSubset(nums, index+1, current, res);
+        curr.push_back(nums[i]);
+        backtrack(nums, i+1, curr, res);
+        curr.pop_back();
+        backtrack(nums, i+1, curr, res);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> current;
-        createSubset(nums, 0, current, res);
+        vector<int> curr;
+        backtrack(nums, 0, curr, res);
         return res;
     }
 };
