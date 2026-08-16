24a1c30e9d3185f7b8997e402ff128a26c84d64b
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-16 14:58:04 +0530

    Time: 2 ms (25.27%), Space: 15 MB (23.63%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -6,35 +6,24 @@ public:
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         queue<pair<int, int>> q;
         for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(board[0][j] == 'O') {
-                    q.push({0, j});
-                }
-                if(board[m-1][j] == 'O') {
-                    q.push({m-1, j});
-                }
-                if(board[i][0] == 'O') {
-                    q.push({i, 0});
-                }
-                if(board[i][n-1] == 'O') {
-                    q.push({i, n-1});
-                }
-            }
+            if(board[i][0] == 'O') q.push({i, 0});
+            if(board[i][n-1] == 'O') q.push({i, n-1});
         }
-        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        for(int j=0;j<n;j++) {
+            if(board[0][j] == 'O') q.push({0, j});
+            if(board[m-1][j] == 'O') q.push({m-1, j});
+        }
+        vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         while(!q.empty()) {
-            int size = q.size();
-            for(int i=0;i<size;i++) {
-                auto[r, c] = q.front();
-                visited[r][c] = true;
-                q.pop();
-                for(auto [dr, dc] : dir) {
-                    int nr = r + dr;
-                    int nc = c + dc;
-                    if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && board[nr][nc] == 'O') {
-                        visited[nr][nc] = true;
-                        q.push({nr, nc});
-                    }
+            auto [r, c] = q.front();
+            q.pop();
+            visited[r][c] = true;
+            for(auto [dr, dc] : dir) {
+                int nr = r + dr;
+                int nc = c + dc;
+                if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && board[nr][nc] == 'O') {
+                    q.push({nr, nc});
+                    visited[nr][nc] = true;
                 }
             }
         }
