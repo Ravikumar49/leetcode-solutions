3973e0d72a2d450a0c074022e4fe6fac9872988c
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-16 18:20:50 +0530

    Time: 3 ms (25.73%), Space: 17.3 MB (10.63%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -7,33 +7,39 @@ public:
         int totalFresh = 0;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(grid[i][j] == 2) q.push({i, j});
-                else if(grid[i][j] == 1) totalFresh++;
+                if(grid[i][j] == 2) {
+                    q.push({i, j});
+                }
+                else if(grid[i][j] == 1) {
+                    totalFresh++;
+                }
             }
         }
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int invaded = 0;
         int time = 0;
+        int invaded = 0;
         while(!q.empty()) {
             int size = q.size();
             bool spread = false;
             for(int i=0;i<size;i++) {
                 auto [r, c] = q.front();
+                visited[r][c] = true;
                 q.pop();
                 for(auto [dr, dc] : dir) {
                     int nr = r + dr;
                     int nc = c + dc;
-                    if(nr < m && nc < n && nr >= 0 && nc >= 0 && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2;
-                        q.push({nr, nc});
+                    if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && grid[nr][nc] == 1) {
                         invaded++;
+                        visited[nr][nc] = true;
                         spread = true;
+                        q.push({nr, nc});
                     }
                 }
             }
             if(spread) time++;
         }
-        if(invaded != totalFresh) return -1;
-        return time;
+        if(invaded == totalFresh) return time;
+        return -1;
     }
 };
