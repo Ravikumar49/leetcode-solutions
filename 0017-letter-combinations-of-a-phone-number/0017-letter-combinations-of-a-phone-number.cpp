5408532724661e90dc1a33c1061e8f3231513ae1
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-18 13:52:45 +0530

    Time: 2 ms (15.5%), Space: 9.1 MB (90.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void backtrack(string& digits, int index, unordered_map<char, string>& mp, string curr, vector<string>& res) {
+    void backtrack(string& digits, int index, unordered_map<char, string>& mp, string& curr, vector<string>& res) {
         if(index == digits.size()) {
             res.push_back(curr);
             return;
         }
-        for(auto c : mp[digits[index]]) {
+        for(char c : mp[digits[index]]) {
             curr.push_back(c);
             backtrack(digits, index+1, mp, curr, res);
             curr.pop_back();
