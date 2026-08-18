757bf3b8dad04ddf48314584f166f53893c7c9ad
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-18 09:18:06 +0530

    Time: 2 ms (57.89%), Space: 29.1 MB (78.95%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n == k) return *max_element(nums.begin(), nums.end());
+        int count[51] = {0};
+        for(int x : nums) count[x]++;
         if(k == 1) {
-            unordered_map<int, int> freq;
-            for(int i=0;i<nums.size();i++) {
-                freq[nums[i]]++;
+            for(int i=50;i>=0;i--) {
+                if(count[i] == 1) return i;
             }
-            int ans = -1;
-            for(auto x : freq) {
-                if(x.second == 1 && x.first > ans) {
-                    ans = x.first;
-                }
-            }
-            return ans;
+            return -1;
         }
-        else if(k == nums.size()) {
-            return *max_element(nums.begin(), nums.end());
+        int res = -1;
+        if(count[nums[0]] == 1) {
+            res = max(res, nums[0]);
         }
-        else {
-            int n = nums.size();
-            unordered_map<int, int> freq;
-            for(int i=0;i<n;i++) {
-                freq[nums[i]]++;
-            }
-            int ans = -1;
-            if(freq[nums[0]] == 1 && freq[nums[n-1]] == 1) {
-                return max(nums[0], nums[n - 1]);
-            }
-            if(freq[nums[0]] > 1 && freq[nums[n-1]] == 1) {
-                return nums[n-1];
-            }
-            else if(freq[nums[n-1]] > 1 && freq[nums[0]] == 1) {
-                return nums[0];
-            }
-            else {
-                return -1;
-            }
+        if(count[nums.back()] == 1) {
+            res = max(res, nums.back());
         }
-        return -1;
+        return res;
     }
 };
