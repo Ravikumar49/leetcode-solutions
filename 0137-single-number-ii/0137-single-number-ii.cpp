90cf52ddf567acf469e5d6a4f4797e5363183176
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-19 22:55:21 +0530

    Time: 3 ms (31.96%), Space: 13.3 MB (87.5%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int bit=0;bit<32;bit++) {
+            int sum = 0;
+            int mask = 1 << bit;
+            for(int i=0;i<n;i++) {
+                if((nums[i] & mask) != 0) sum++;
+            }
+            if(sum%3 == 1) ans |= 1 << bit;
+        }
+        return ans;
+    }
+};
