09b841f135f0800892ef3951eaac13936d4d2241
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-21 19:14:22 +0530

    Time: 4 ms (14%), Space: 14.6 MB (76.19%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> curr = {0};
-        for(int bits=1;bits<=n;bits++) {
-            int add = 1 << (bits-1);
+        for(int bit=1;bit<=n;bit++) {
+            int add = 1 << (bit-1);
             int size = curr.size();
             for(int i=size-1;i>=0;i--) {
                 curr.push_back(curr[i] + add);
-            } 
+            }
         }
         return curr;
     }
