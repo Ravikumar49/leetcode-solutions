fe700194b7fdbd7440042b248053fdb0129f7e13
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-21 10:39:33 +0530

    Time: 7 ms (93.02%), Space: 21.9 MB (11.63%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    long long count(vector<int>& coins, long long x) {
+    long long count(vector<long long>& subsetLCM, vector<int>& parity, long long x) {
         long long c = 0;
-        for(int mask=1;mask<(1<<coins.size());mask++) {
-            long long lc = 1;
-            int selected = 0;
-            for(int j=0;j<coins.size();j++) {
-                if(mask & (1 << j)) {
-                    selected++;
-                    lc = lcm(lc, (long long)coins[j]);
-                }
-            }
-            long long contribution = x/lc;
-            if(selected % 2 == 1) c += contribution;
-            else c -= contribution;
+        for(int mask=1;mask<subsetLCM.size();mask++) {
+            if(subsetLCM[mask] > x) continue;
+            long long contribution = x / subsetLCM[mask];
+            if(parity[mask]) c+= contribution;
+            else c-= contribution;
         }
         return c;
     }
     long long findKthSmallest(vector<int>& coins, int k) {
-        long long lo = *min_element(coins.begin(), coins.end());
-        long long hi = k * lo;
-        while(lo < hi) {
+        int n = coins.size();
+        int totalMasks = 1 << n;
+        vector<long long> subsetLCM(totalMasks);
+        vector<int> parity(totalMasks);
+
+        subsetLCM[0] = 1;
+        parity[0] = 0;
+        
+        for(int mask=1;mask<totalMasks;mask++) {
+            int bit = __builtin_ctz(mask);
+            int prev = mask & (mask - 1);
+
+            subsetLCM[mask] = lcm(subsetLCM[prev], (long long)coins[bit]);
+            parity[mask] = parity[prev] ^ 1;
+        }
+        long long lo = *min_element(coins.begin(), coins.end()); long long hi = k * lo;
+        while(lo < hi) { 
             long long mid = lo + (hi - lo)/2;
-            if(count(coins, mid) >= k) {
+            if(count(subsetLCM, parity, mid) >= k) { 
                 hi = mid;
             }
             else {
