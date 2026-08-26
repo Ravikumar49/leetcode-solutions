0456cb65b52dd18448a31356df8a1d217ae05b42
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-26 22:12:20 +0530

    Time: 58 ms (35.83%), Space: 66.8 MB (40.63%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         unordered_map<int, int> rowMask;
-        for(auto seats : reservedSeats) {
-            int row = seats[0];
-            int col = seats[1];
+        for(auto r : reservedSeats) {
+            int row = r[0];
+            int col = r[1];
             rowMask[row] |= (1 << col);
         }
-        int groupA, groupB, groupC;
-        groupA = (1 << 9) | (1 << 8) | (1 << 7) | (1 << 6);
-        groupB = (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4);
-        groupC = (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2);
+        int groupA = (1 << 9) | (1 << 8) | (1 << 7) | (1 << 6);
+        int groupB = (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4);
+        int groupC = (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2);
         int total = 0;
         for(auto [r, k] : rowMask) {
-            if((k & groupA) == 0 && (k & groupC) == 0) total += 2;
-            else if((k & groupA) == 0 || (k & groupC) == 0) total++;
-            else if((k & groupB) == 0) total++;
+            if(((groupA & k) == 0) && ((groupC & k) == 0)) total += 2;
+            else if (((groupA & k) == 0) || ((groupC & k) == 0)) total++;
+            else if((groupB & k) == 0) total++;
         }
         total += 2 * (n - rowMask.size());
         return total;
