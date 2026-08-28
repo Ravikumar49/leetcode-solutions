f36762efbae34b3ba7cebc75ff4757dbb97a0c09
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-28 17:08:51 +0530

    Time: 104 ms (33.36%), Space: 22.1 MB (23.1%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string, vector<string>> parents;
         unordered_map<string, int> distance;
         unordered_set<string> wordSet(wordList.begin(), wordList.end());
         distance[beginWord] = 0;
@@ -23,11 +22,7 @@ public:
                         if(!wordSet.count(current)) continue;
                         if(!distance.count(current)) {
                             distance[current] = distance[parent] + 1;
-                            parents[current].push_back(parent);
                             q.push(current);
-                        }
-                        else if(distance[current] == distance[parent] + 1) {
-                            parents[current].push_back(parent);
                         }
                     }
                     parent[index] = original;
