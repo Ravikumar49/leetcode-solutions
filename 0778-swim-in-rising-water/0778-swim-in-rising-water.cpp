ab19e436b2ceb806b586b5d0e3a44a5ccf1e62bd
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-08-29 22:41:01 +0530

    Time: 12 ms (31.07%), Space: 14.4 MB (36.11%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canReach(vector<vector<int>>& grid, int t) {
+        int n = grid.size();
+        if(grid[0][0] > t) return false;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        while(!q.empty()) {
+            int size = q.size();
+            for(int i=0;i<size;i++) {
+                auto [r, c] = q.front();
+                q.pop();
+                if(r == n-1 && c == n-1) return true;
+                visited[r][c] = true;
+                for(auto [dr, dc] : dir) {
+                    int nr = r + dr;
+                    int nc = c + dc;
+                    if(nr < n && nr >= 0 && nc < n && nc >= 0 && !visited[nr][nc] && grid[nr][nc] <= t) {
+                        q.push({nr, nc});
+                        visited[nr][nc] = true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int left = 0;
+        int right = n * n - 1;
+        while(left < right) {
+            int mid = left + (right - left)/2;
+            if(canReach(grid, mid)) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
