591c483bf32a0d9d012a323e0180cbc9e2befc50
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-02 00:20:10 +0530

    Time: 1 ms (36.06%), Space: 17.2 MB (16.72%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -7,32 +7,28 @@ public:
         int totalFresh = 0;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(grid[i][j] == 2) {
-                    q.push({i, j});
-                }
-                else if(grid[i][j] == 1) {
-                    totalFresh++;
-                }
+                if(grid[i][j] == 2) q.push({i, j});
+                else if(grid[i][j] == 1) totalFresh++;
             }
         }
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int time = 0;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         int invaded = 0;
+        int time = 0;
         while(!q.empty()) {
             int size = q.size();
             bool spread = false;
             for(int i=0;i<size;i++) {
-                auto [r, c] = q.front();
+                auto[r, c] = q.front();
                 visited[r][c] = true;
                 q.pop();
                 for(auto [dr, dc] : dir) {
                     int nr = r + dr;
                     int nc = c + dc;
-                    if(nr < m && nc < n && nr >= 0 && nc >= 0 && !visited[nr][nc] && grid[nr][nc] == 1) {
+                    if(nr < m && nr >= 0 && nc < n && nc >= 0 && !visited[nr][nc] && grid[nr][nc] == 1) {
                         invaded++;
-                        visited[nr][nc] = true;
                         spread = true;
+                        visited[nr][nc] = true;
                         q.push({nr, nc});
                     }
                 }
