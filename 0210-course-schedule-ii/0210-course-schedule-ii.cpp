2f10185bea687dc0366b91f512ea51e1c3ffc4b0
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-02 11:14:01 +0530

    Time: 7 ms (33.72%), Space: 18.9 MB (28.81%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, vector<int>& states, int course, vector<int>& ans) {
+    bool dfs(vector<int>& states, int course, vector<vector<int>>& adj, vector<int>& ans) {
         if(states[course] == 1) return true;
         if(states[course] == 2) return false;
         states[course] = 1;
         for(int c : adj[course]) {
-            if(dfs(adj, states, c, ans)) return true;
+            if(dfs(states, c, adj, ans)) return true;
         }
         ans.push_back(course);
         states[course] = 2;
@@ -13,15 +13,15 @@ public:
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> states(numCourses, 0);
-        vector<vector<int>> adj(numCourses);
-        for(auto &p : prerequisites) {
+        vector<vector<int>> adj(numCourses + 1);
+        vector<int> ans;
+        for(auto p : prerequisites) {
             int a = p[0];
             int b = p[1];
             adj[a].push_back(b);
         }
-        vector<int> ans;
         for(int i=0;i<numCourses;i++) {
-            if(dfs(adj, states, i, ans)) return {};
+            if(dfs(states, i, adj, ans)) return {};
         }
         return ans;
     }
