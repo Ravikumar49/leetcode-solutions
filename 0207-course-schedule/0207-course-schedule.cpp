c9ec70b57aa4bf4c27e3acbab7fa2b6176dcfc37
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-02 11:08:14 +0530

    Time: 8 ms (35.23%), Space: 19.9 MB (39.99%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, vector<int>& states, int courses) {
-        if(states[courses] == 1) return true;
-        if(states[courses] == 2) return false;
-        states[courses] = 1;
-        for(int c : adj[courses]) {
-            if(dfs(adj, states, c)) return true;
+    bool dfs(vector<vector<int>>& adj, int course, vector<int>& states) {
+        if(states[course] == 1) return true;
+        if(states[course] == 2) return false;
+        states[course] = 1;
+        for(int c : adj[course]) {
+            if(dfs(adj, c, states)) return true;
         }
-        states[courses] = 2;
+        states[course] = 2;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> states(numCourses, 0);
-        vector<vector<int>> adj(numCourses);
-        for(auto &p : prerequisites) {
-            int a = p[0];
-            int b = p[1];
+        vector<vector<int>> adj(numCourses + 1);
+        for(auto e : prerequisites) {
+            int a = e[0];
+            int b = e[1];
             adj[a].push_back(b);
         }
         for(int i=0;i<numCourses;i++) {
-            if(dfs(adj, states, i)) return false;
+            if(dfs(adj, i, states)) return false;
         }
         return true;
     }
