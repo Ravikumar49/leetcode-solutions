15356d11172307c95f30dfdfe369887a3c667a19
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-02 10:01:03 +0530

    Time: 63 ms (25.39%), Space: 165.9 MB (32.12%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -4,12 +4,10 @@ public:
         int n = nums1.size();
         sort(nums1.begin(), nums1.end());
         int parity = nums1[0] % 2;
-        bool seenOdd = nums1[0]%2 == 1 ? true : false;
+        bool seenOdd = (nums1[0]%2 != 0);
         for(int i=1;i<n;i++) {
             if(nums1[i] % 2 == parity) continue;
-            else {
-                if(!seenOdd) return false;
-            }
+            if(!seenOdd) return false;
             if(nums1[i] % 2 == 1) seenOdd = true;
         }
         return true;
