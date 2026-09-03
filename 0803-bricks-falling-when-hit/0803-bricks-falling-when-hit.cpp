0c61d3fa6697d67fcebcb4eab09a0c4cd6fa4c9f
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-03 17:01:29 +0530

    Time: 66 ms (26.51%), Space: 125.9 MB (18.68%) - LeetHub

--- 0803-bricks-falling-when-hit/0803-bricks-falling-when-hit.cpp
+++ 0803-bricks-falling-when-hit/0803-bricks-falling-when-hit.cpp
@@ -18,10 +18,8 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> original = grid;
-        for(auto r : hits) {
-            int row = r[0];
-            int col = r[1];
-            grid[row][col] = 0;
+        for(auto hit : hits) {
+            grid[hit[0]][hit[1]] = 0;
         }
         parent.resize(m*n + 1);
         sz.resize(m*n + 1);
@@ -29,32 +27,32 @@ public:
             parent[i] = i;
             sz[i] = 1;
         }
-        for(int r=0;r<m;r++) {
-            for(int c=0;c<n;c++) {
-                if(grid[r][c] == 0) continue;
-                int current = r * n + c;
-                if(r == 0) unite(current, m*n);
-                if(r+1<m && grid[r+1][c] == 1) unite(current, (r+1)*n + c);
-                if(c+1<n && grid[r][c+1] == 1) unite(current, r * n + (c+1));
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == 0) continue;
+                int current = i * n + j;
+                if(i == 0) unite(current, m*n);
+                if(i+1 < m && grid[i+1][j] == 1) unite(current, (i+1) * n + j);
+                if(j+1 < n && grid[i][j+1] == 1) unite(current, i * n + (j + 1));
             }
         }
-        vector<pair<int, int>> dir = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
         vector<int> res(hits.size());
+        vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         for(int i=hits.size()-1;i>=0;i--) {
-            int row = hits[i][0];
-            int col = hits[i][1];
-            if(original[row][col] == 0 || grid[row][col] == 1) {
+            int r = hits[i][0];
+            int c = hits[i][1];
+            if(original[r][c] == 0 || grid[r][c] == 1) {
                 res[i] = 0;
                 continue;
             }
             int before = sz[find(m*n)];
-            grid[row][col] = 1;
-            int current = row * n + col;
-            if(row == 0) unite(current, m*n);
+            grid[r][c] = 1;
+            int current = r * n + c;
+            if(r == 0) unite(current, m*n);
             for(auto [dr, dc] : dir) {
-                int nr = row + dr;
-                int nc = col + dc;
-                if(nr < m && nr >=0 && nc < n && nc >= 0 && grid[nr][nc] == 1) unite(current, nr*n + nc);
+                int nr = r + dr;
+                int nc = c + dc;
+                if(nr < m && nr >= 0 && nc < n && nc >= 0 && grid[nr][nc] == 1) unite(current, nr * n + nc);
             }
             int after = sz[find(m*n)];
             res[i] = max(0, after - before - 1);
