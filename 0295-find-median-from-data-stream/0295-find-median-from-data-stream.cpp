f498e4235ef79c2ffa3918457b3a8c7b43f84501
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-08 22:07:53 +0530

    Time: 82 ms (40.68%), Space: 148.6 MB (64.2%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -9,13 +9,13 @@ public:
     void addNum(int num) {
         if(left.empty() || num <= left.top()) left.push(num);
         else right.push(num);
-        if(left.size() > right.size() + 1) {
-            right.push(left.top());
-            left.pop();
-        }
-        else if(right.size() > left.size()) {
+        if(left.size() < right.size()) {
             left.push(right.top());
             right.pop();
+        }
+        else if(left.size() > right.size() + 1) {
+            right.push(left.top());
+            left.pop();
         }
     }
     
