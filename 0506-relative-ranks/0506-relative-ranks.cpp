40e176562a6d76b4340421954b090ef068c242f5
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-08 15:54:43 +0530

    Time: 6 ms (45.28%), Space: 15.6 MB (47.93%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -5,17 +5,16 @@ public:
         for(int i=0;i<score.size();i++) pq.push({score[i], i});
         vector<string> ans(score.size());
         for(int count=0;count<3 && !pq.empty();count++) {
-            auto [val, index] = pq.top();
-            if(count == 0) ans[index] = "Gold Medal";
-            else if(count == 1) ans[index] = "Silver Medal";
-            else ans[index] = "Bronze Medal";
+            if(count == 0) ans[pq.top().second] = "Gold Medal";
+            else if(count == 1) ans[pq.top().second] = "Silver Medal";
+            else ans[pq.top().second] = "Bronze Medal";
             pq.pop();
         }
         int pos = 4;
         while(!pq.empty()) {
             auto [val, index] = pq.top();
-            ans[index] = to_string(pos);
             pq.pop();
+            ans[index] = to_string(pos);
             pos++;
         }
         return ans;
