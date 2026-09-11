524239c88890048149219289d9da548cf644abf9
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-11 09:19:42 +0530

    Time: 3 ms (84.06%), Space: 29.4 MB (80.99%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        unordered_map<int, int> freq;
+        bool isEven = false;
+        for(int x : digits) {
+            if(x%2 == 0) isEven = true;
+            freq[x]++;
+        }
+        if(!isEven) return 0;
+        int ans = 0;
+        int val, curr1, curr2, curr3;
+        for(int units=0;units<=8;units+=2) {
+            if(freq[units] > 0) {
+                val = units;
+                curr1 = val;
+                freq[units]--;
+                for(int tens=0;tens<=9;tens++) {
+                    if(freq[tens] > 0) {
+                        val = (tens*10) + val;
+                        curr2 = val;
+                        freq[tens]--;
+                        for(int hundred=1;hundred<=9;hundred++) {
+                            if(freq[hundred] > 0) {
+                                val = (hundred*100) + val;
+                                curr3 = val;
+                                freq[hundred]--;
+                                if(val <= 998) {
+                                    ans++;
+                                }
+                                freq[hundred]++;
+                                val = curr2;
+                            }
+                        }
+                        freq[tens]++;
+                        val = curr1;
+                    }
+                }
+                freq[units]++;
+            }
+        }
+        return ans;
+    }
+};
