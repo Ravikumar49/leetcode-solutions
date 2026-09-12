a257c5e9326825a51c858392a917bd5dd78ffc9c
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-12 22:30:12 +0530

    Time: 0 ms (100%), Space: 15.1 MB (40.6%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -17,11 +17,11 @@ public:
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()) {
-            int levelSize = q.size();
-            for(int i=0;i<levelSize;i++) {
+            int size = q.size();
+            for(int i=0;i<size;i++) {
                 TreeNode* curr = q.front();
                 q.pop();
-                if(i == levelSize - 1) result.push_back(curr->val);
+                if(i == size - 1) result.push_back(curr->val);
                 if(curr->left != nullptr) q.push(curr->left);
                 if(curr->right != nullptr) q.push(curr->right);
             }
