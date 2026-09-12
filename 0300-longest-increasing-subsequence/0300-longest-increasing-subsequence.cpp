c57c65e052755debc270f89b9b4c4ac1ab03fa4c
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-12 22:26:21 +0530

    Time: 74 ms (50.07%), Space: 14.4 MB (48.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,15 +5,11 @@ public:
         vector<int> dp(n, 1);
         for(int i=1;i<n;i++) {
             for(int j=0;j<i;j++) {
-                if(nums[j] < nums[i]) {
-                    dp[i] = max(1 + dp[j], dp[i]);
-                }
+                if(nums[j] < nums[i]) dp[i] = max(1 + dp[j], dp[i]);
             }
         }
         int ans = -1;
-        for(int x : dp) {
-            ans = max(x, ans);
-        }
+        for(int x : dp) ans = max(ans, x);
         return ans;
     }
 };
