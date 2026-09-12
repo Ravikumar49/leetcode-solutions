a622f8bd226d8abaf8ad736438e4aed47e7c0d1f
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-12 16:37:57 +0530

    Time: 8 ms (85.03%), Space: 8.9 MB (89.86%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int numDistinct(string s, string t) {
         int n = t.size();
-        vector<long long> dp(n + 1, 0);
         const long long LIMIT = INT_MAX + 1LL;
+        vector<long long> dp(n+1, 0);
         dp[0] = 1;
         for(int j=0;j<s.size();j++) {
             for(int i=t.size();i>=1;i--) {
-                if(s[j] == t[i-1]) {
+                if(s[j] == t[i - 1]) {
                     if(dp[i - 1] > LIMIT - dp[i]) dp[i] = LIMIT;
                     else dp[i] += dp[i-1];
                 }
