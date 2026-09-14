1cea85493fb0f8f82ce03d6ea9860098378f1813
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-14 15:43:17 +0530

    Time: 2 ms (33.62%), Space: 9.7 MB (25.21%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int distinctSubseqII(string s) {
         int n = s.size();
-        vector<int> last(n, -1);
         vector<int> pos(26, -1);
+        vector<int> last(n, -1);
         for(int i=0;i<n;i++) {
             last[i] = pos[s[i] - 'a'];
             pos[s[i] - 'a'] = i;
         }
-        const long long MOD = 1000000007;
         vector<int> dp(n+1, 1);
+        const long long MOD = 1000000007;
         for(int i=1;i<=n;i++) {
             if(last[i-1] == -1) dp[i] = (2LL * dp[i-1]) % MOD;
-            else {
-                dp[i] = ((2 * dp[i-1]) % MOD - dp[last[i-1]] + MOD) % MOD;
-            }
+            else dp[i] = ((2LL * dp[i-1]) % MOD - dp[last[i - 1]] + MOD) % MOD;
         }
         return (dp[n] - 1 + MOD) % MOD;
     }
