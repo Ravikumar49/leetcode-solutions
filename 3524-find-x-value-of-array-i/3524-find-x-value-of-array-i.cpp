b4c16dc7a6b7b2999b09d8b35c3271fc1e3ffbb9
Ravikumar49 <142192227+Ravikumar49@users.noreply.github.com>
2026-09-21 10:31:37 +0530

    Time: 163 ms (50.89%), Space: 151.1 MB (55.36%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     vector<long long> resultArray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<vector<long long>> dp(n, vector<long long>(k, 0));
-        int rem = nums[0] % k;
-        dp[0][rem] = 1;
-        for(int i=1;i<n;i++) {
+        vector<long long> dp(k, 0);
+        vector<long long> result(k);
+        for(int i=0;i<n;i++) {
+            vector<long long> newDp(k);
             for(int r=0;r<k;r++) {
                 long long newRemainder = (1LL * r * nums[i]) % k;
-                dp[i][newRemainder] += dp[i-1][r];
+                newDp[newRemainder] += dp[r];
             }
-            dp[i][nums[i] % k] += 1;
-        }
-        vector<long long> result(k, 0);
-        for(int r=0;r<k;r++) {
-            for(int i=0;i<n;i++) {
-                result[r] += dp[i][r];
-            }
+            long long rem = nums[i] % k;
+            newDp[rem] += 1;
+            for(int r=0;r<k;r++) result[r] += newDp[r];
+            dp = newDp;
         }
         return result;
     }
